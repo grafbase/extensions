459ba262dd9bf86e29a9f00325aa5af73d436165
Julius de Bruijn <julius@nauk.io>
2025-05-07 10:59:48 +0000

    feat(gb-9000): fix cli run without .env and config file

--- cli/postgres/src/main.rs
+++ cli/postgres/src/main.rs
@@ -6,7 +6,7 @@ mod args;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> anyhow::Result<()> {
-    dotenv::dotenv()?;
+    dotenv::dotenv().ok();
 
     let args = args::parse();
     let mut conn = PgConnection::connect(&args.database_url).await?;
@@ -21,7 +21,11 @@ async fn main() -> anyhow::Result<()> {
 }
 
 async fn introspect(conn: &mut PgConnection, cmd: IntrospectCommand) -> anyhow::Result<()> {
-    let config = std::fs::read_to_string(&cmd.config).context("could not read the configuration file")?;
+    let config = std::fs::read_to_string(&cmd.config).context(concat!(
+        "Could not find a configuration file. Please provide a valid path ",
+        "with --config, or make sure file ./grafbase-postgres.toml exists."
+    ))?;
+
     let config = toml::from_str(&config)?;
     let sdl = grafbase_postgres_introspection::introspect(conn, config).await?;
 
